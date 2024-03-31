38e45314b18d5669fe9949ec5aa02bc82aa4eea8
CtrliPhones <62081155+CtrliPhones@users.noreply.github.com>
2024-03-30 20:57:41 -0400

    Updates BrowserView to 1.8

--- Weave/BrowserView.swift
+++ Weave/BrowserView.swift
@@ -4,7 +4,7 @@
 import SwiftUI
 import WebKit
 
-// Fetches Accent Color values to use within CSS code
+// Fetches Accent Color values to use within CSS code, future usage I swear.
 extension NSColor {
     var hexString: String {
         guard let rgbColor = usingColorSpace(NSColorSpace.sRGB) else { return "#000000" }
@@ -22,137 +22,150 @@ extension NSColor {
 // Sets up BrowserView and does basic customizations
 struct BrowserView: View {
     @State private var webView = WKWebView()
-    @State private var urlString = ""
+    @State private var URLString = ""
     @State private var pageTitle = "Weave"
     @State private var faviconImage: NSImage?
     @State private var userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.3.1 Safari/605.1.15"
 
     var body: some View {
-        VStack(spacing: 0) {
-            Divider()
+        WebView(webView: webView, pageTitle: $pageTitle, URLString: $URLString, faviconImage: $faviconImage, userAgent: $userAgent)
+            .onAppear {
+                // CSS Style Injection
+                var sysAccent = NSColor.systemAccentColor.hexString
+                let styleSheet = """
+                    var style = document.createElement('style');
+                    style.innerHTML = `
+                        * {
+                            font-family: -apple-system !important;
+                        }
+                    `;
+                    document.head.appendChild(style);
+                """
 
-            WebView(webView: webView, pageTitle: $pageTitle, urlString: $urlString, faviconImage: $faviconImage, userAgent: $userAgent)
-                .onAppear {
-                    // CSS Style Injection
-                    let sysAccent = NSColor.systemAccentColor.hexString
-                    let userScript = """
-                        var style = document.createElement('style');
-                        style.innerHTML = `
-                            body, p, h1, h2, h3, h4, h5, h6, ul, ol, li, a {
-                                font-family: -apple-system !important;
-                            }
-                        `;
-                        document.head.appendChild(style);
-                    """
+                // Extremely basic CSS Adblock Injection
+                let adBlockerScript = """
+                    var adBlockStyle = document.createElement('style');
+                    adBlockStyle.innerHTML = `
+                        /* Hide common ad classes */
+                        .ad-banner, .ad-wrapper, .ad-container, .ad, .ads, .adsense, .adslot, .ad-badge {
+                            display: none !important;
+                        }
 
-                    // Simplistic Adblocking Injection
-                    let adBlockerScript = """
-                        var adBlockStyle = document.createElement('style');
-                        adBlockStyle.innerHTML = `
-                            /* Hide common ad classes */
-                            .ad-banner, .ad-wrapper, .ad-container, .ad, .ads, .adsense, .adslot, .ad-badge {
-                                display: none !important;
-                            }
+                        /* Hide ads from specific URLs */
+                        [href*="doubleclick.net"], [href*="googleadservices.com"], [href*="advertising.com"], [src*="adserver.com"] {
+                            display: none !important;
+                        }
+                    `;
+                    document.head.appendChild(adBlockStyle);
+                """
+                let adBlockInject = WKUserScript(source: adBlockerScript, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+                webView.configuration.userContentController.addUserScript(adBlockInject)
 
-                            /* Hide ads from specific URLs */
-                            [href*="doubleclick.net"], [href*="googleadservices.com"], [href*="advertising.com"], [src*="adserver.com"] {
-                                display: none !important;
-                            }
-                        `;
-                        document.head.appendChild(adBlockStyle);
-                    """
-                    let adBlockScript = WKUserScript(source: adBlockerScript, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
-                    webView.configuration.userContentController.addUserScript(adBlockScript)
-
-                    let userScriptInjection = WKUserScript(source: userScript, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
-                    webView.configuration.userContentController.addUserScript(userScriptInjection)
-                }
-        }
-        // Toolbar and navigation stuff
-        .frame(minWidth: 1000, minHeight: 600)
-        .navigationTitle("")
-        .toolbar {
-            ToolbarItem(placement: .navigation) {
-                Button(action: goBack) {
-                    Image(systemName: "chevron.left")
-                }
-                .help("Go back")
+                let CSSInject = WKUserScript(source: styleSheet, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
+                webView.configuration.userContentController.addUserScript(CSSInject)
+                
+                // Loads URL when app opens
+                loadURL()
             }
-            
-            ToolbarItem(placement: .navigation) {
-                Button(action: goForward) {
-                    Image(systemName: "chevron.right")
-                }
-                .help("Go forward")
-            }
-            
-            ToolbarItem(placement: .primaryAction) {
-                TextField("Search or enter URL", text: $urlString, onCommit: loadUrl)
-                    .textFieldStyle(RoundedBorderTextFieldStyle())
-                    .frame(width: 400, height: nil) // Set a fixed width
-                    .multilineTextAlignment(.center)
-                    .lineLimit(1) // Limit to 1 line
-                    .truncationMode(.tail) // Truncates at the end
-                    .onAppear {
-                        self.urlString = webView.url?.absoluteString ?? ""
+        
+            // Sets minimum sizing for window and defines toolbar title's content
+            .frame(minWidth: 1000, minHeight: 600)
+            .navigationTitle(pageTitle)
+        
+            // Toolbar with keyboard shortcuts and tooltips
+            .toolbar {
+                // Back button
+                ToolbarItem(placement: .navigation) {
+                    Button(action: goBack) {
+                        Image(systemName: "chevron.left")
                     }
-                    .help("Enter a URL or search term")
-            }
-            
-            ToolbarItem(placement: .primaryAction) {
-                Button(action: loadUrl) {
-                    Image(systemName: "arrow.up.circle.fill")
+                    .help("Go back")
+                    .keyboardShortcut(KeyEquivalent.leftArrow, modifiers: [.command])
+                    .disabled(!webView.canGoBack)
                 }
-                .help("Begin loading page")
-            }
-            
-            ToolbarItem(placement: .status) {
-                if let faviconImage = faviconImage {
-                    Image(nsImage: faviconImage)
-                        .resizable()
-                        .frame(width: 18, height: 18) // Size set to 18x18 for favicon
-                } else {
-                    Image(systemName: "link")
-                        .resizable()
-                        .frame(width: 18, height: 18) // Placeholder icon
+                
+                // Forward button
+                ToolbarItem(placement: .navigation) {
+                    Button(action: goForward) {
+                        Image(systemName: "chevron.right")
+                    }
+                    .help("Go forward")
+                    .keyboardShortcut(KeyEquivalent.rightArrow, modifiers: [.command])
+                    .disabled(!webView.canGoForward)
+                }
+                
+                // Address Bar
+                ToolbarItem(placement: .status) {
+                    TextField("Search or enter URL", text: $URLString, onCommit: loadURL)
+                        .textFieldStyle(RoundedBorderTextFieldStyle())
+                        .frame(width: 500, height: nil) // Set a fixed width
+                        .multilineTextAlignment(.center)
+                        .lineLimit(1) // Limit to 1 line
+                        .truncationMode(.tail) // Truncates at the end
+                        .onAppear {
+                            self.URLString = webView.url?.absoluteString ?? ""
+                        }
+                        .help("Enter a URL or search term")
+                }
+                
+                // Refresh button
+                ToolbarItem(placement: .navigation) {
+                    Button(action: refresh) {
+                        Image(systemName: "arrow.clockwise")
+                    }
+                    .help("Refresh this page")
+                    .keyboardShortcut("r", modifiers: [.command])
+                }
+                
+                // Favicon display
+                ToolbarItem(placement: .navigation) {
+                    if let faviconImage = faviconImage {
+                        Image(nsImage: faviconImage)
+                            .resizable()
+                            .frame(width: 18, height: 18) // Size set to 18x18 for favicon
+                    } else {
+                        Image(systemName: "globe.americas.fill")
+                            .resizable()
+                            .frame(width: 18, height: 18) // Placeholder icon
+                            .font(Font.title.weight(.bold))
+                    }
+                }
+                
+                // Spacer between address bar and next button chunk
+                ToolbarItem(placement: .primaryAction) {
+                    Spacer()
+                }
+                
+                // Share button
+                ToolbarItem(placement: .primaryAction) {
+                    Button(action: {
+                        shareURL(URLString)
+                    }) {
+                        Image(systemName: "square.and.arrow.up")
+                    }
+                    .help("Share this page")
                 }
             }
-            
-            ToolbarItem(placement: .status) {
-                Text(pageTitle)
-                    .font(.headline)
-                    .lineLimit(1)
-            }
-            
-            ToolbarItem(placement: .status) {
-                Spacer()
-            }
-            
-            ToolbarItem(placement: .navigation) {
-                Button(action: refresh) {
-                    Image(systemName: "arrow.clockwise")
-                }
-                .help("Reload this page")
-            }
-        }
-        .onAppear {
-            if let userAgent = webView.value(forKey: "userAgent") as? String {
-                self.userAgent = userAgent.replacingOccurrences(of: "Mobile", with: "Safari")
-            }
-            loadUrl()
-        }
     }
 
-    // Basic functions
+    // Sets up functions for webpage commands
     private func goBack() {
         if webView.canGoBack {
             webView.goBack()
+            // Adds extremely slight delay before refreshing page when navigating
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                refresh()
+            }
         }
     }
 
     private func goForward() {
         if webView.canGoForward {
             webView.goForward()
+            // Adds extremely slight delay before refreshing page when navigating
+            DispatchQueue.main.asyncAfter(deadline: .now() + 0.1) {
+                refresh()
+            }
         }
     }
 
@@ -160,11 +173,23 @@ struct BrowserView: View {
         webView.reload()
     }
 
-    private func loadUrl() {
-        let trimmedUrlString = urlString.trimmingCharacters(in: .whitespacesAndNewlines)
-        if trimmedUrlString.contains(".") {
+    private func shareURL(_ URLString: String) {
+        guard let url = URL(string: URLString) else {
+            print("Invalid URL")
+            return
+        }
+
+        let items: [Any] = [url]
+        let sharingServicePicker = NSSharingServicePicker(items: items)
+        
+        sharingServicePicker.show(relativeTo: webView.bounds, of: webView, preferredEdge: .minY)
+    }
+    
+    private func loadURL() {
+        let trimmedURLString = URLString.trimmingCharacters(in: .whitespacesAndNewlines)
+        if trimmedURLString.contains(".") {
             // Contains a dot, likely a URL
-            if let url = URL(string: addHttpIfNeeded(trimmedUrlString)) {
+            if let url = URL(string: addHttpIfNeeded(trimmedURLString)) {
                 let request = URLRequest(url: url)
                 webView.load(request)
             }
@@ -175,26 +200,28 @@ struct BrowserView: View {
     }
 
     private func search() {
-        guard let searchQuery = urlString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
+        guard let searchQuery = URLString.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) else { return }
         if let searchURL = URL(string: "https://google.com/search?q=\(searchQuery)") {
             let request = URLRequest(url: searchURL)
             webView.load(request)
         }
     }
 
-    private func addHttpIfNeeded(_ urlString: String) -> String {
-        if urlString.hasPrefix("http://") || urlString.hasPrefix("https://") {
-            return urlString
+    private func addHttpIfNeeded(_ URLString: String) -> String {
+        if URLString.hasPrefix("http://") || URLString.hasPrefix("https://") {
+            return URLString
         } else {
-            return "https://\(urlString)"
+            return "https://\(URLString)"
         }
     }
 }
 
+// I genuinely do not know
+
 struct WebView: NSViewRepresentable {
     let webView: WKWebView
     @Binding var pageTitle: String // Binding for the pageTitle
-    @Binding var urlString: String // Binding for the urlString
+    @Binding var URLString: String // Binding for the URLString
     @Binding var faviconImage: NSImage? // Binding for the favicon image
     @Binding var userAgent: String // Binding for the user agent
 
@@ -211,32 +238,34 @@ struct WebView: NSViewRepresentable {
     }
 
     func makeCoordinator() -> Coordinator {
-        Coordinator(webView: webView, pageTitle: $pageTitle, urlString: $urlString, faviconImage: $faviconImage)
+        Coordinator(webView: webView, pageTitle: $pageTitle, URLString: $URLString, faviconImage: $faviconImage)
     }
 
     class Coordinator: NSObject, WKNavigationDelegate {
         let webView: WKWebView
         @Binding var pageTitle: String // Binding for the pageTitle
-        @Binding var urlString: String // Binding for the urlString
+        @Binding var URLString: String // Binding for the URLString
         @Binding var faviconImage: NSImage? // Binding for the favicon image
 
-        init(webView: WKWebView, pageTitle: Binding<String>, urlString: Binding<String>, faviconImage: Binding<NSImage?>) {
+        init(webView: WKWebView, pageTitle: Binding<String>, URLString: Binding<String>, faviconImage: Binding<NSImage?>) {
             self.webView = webView
             self._pageTitle = pageTitle
-            self._urlString = urlString
+            self._URLString = URLString
             self._faviconImage = faviconImage
         }
+        
+        // Sets up functions to gather webpage information to display in toolbar
 
         func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
             webView.evaluateJavaScript("document.title") { (result, error) in
                 if let title = result as? String {
                     self.pageTitle = title
-                    self.urlString = webView.url?.absoluteString ?? ""
+                    self.URLString = webView.url?.absoluteString ?? ""
                     self.loadFavicon()
                 }
             }
         }
-
+        
         private func loadFavicon() {
             let script = """
                 var favicon = document.querySelector('link[rel="shortcut icon"]') || document.querySelector('link[rel="icon"]');
